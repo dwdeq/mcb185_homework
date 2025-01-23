d3101c3798386d8d8e43f4debed9b1504379e69a
DQ <dwdeq@ucdavis.edu>
2025-01-22 22:03:17 -0800

    revising exam (practice)

--- exam.sh
+++ exam.sh
@@ -1,3 +1,3 @@
-more Code/MCB185/data/A.thaliana.gff | cut -f 7 | grep [+-] | sort | uniq -cut
-more Code/MCB185/data/GBF_Code/MCB185/data/GCF_000005845.2_ASM584v2_genomic.gbff | cut -f 2 | grep "kinase" | sort | uniq -c
-more Code/MCB185/data/GBF_Code/MCB185/data/GCF_000005845.2_ASM584v2_genomic.gbff | cut -f 2 | grep "isomerase" | sort | uniq -c
+gunzip -c Code/MCB185/data/A.thaliana.gff.gz | cut -f 7 | grep [+-] | sort | uniq -c
+gunzip -c Code/MCB185/data/GCF_000005845.2_ASM584v2_genomic.gbff.gz | cut -f 3 | grep "kinase" | wc
+gunzip -c Code/MCB185/data/GCF_000005845.2_ASM584v2_genomic.gbff.gz | cut -f 3 | grep "isomerase" | wc
